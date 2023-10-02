a1a0ab639ac2eb7d6e9f28f839909706e254cd77
gabrielemafrica <gabriele.ga@live.it>
2023-10-02 18:24:31 +0300

    test

---
+++ src/Acme/UdemyBundle/AcmeUdemyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\UdemyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeUdemyBundle extends Bundle
+{
+}
---
+++ src/Acme/UdemyBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Acme\UdemyBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
